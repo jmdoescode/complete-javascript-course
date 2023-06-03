2c4699a21eaea618a288b876757f9c2d8a25fd30
jm codes <jmdoescode@gmail.com>
2023-06-02 17:24:36 -0700

    234. Displaying a Map Marker

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -17,18 +17,27 @@ if(navigator.geolocation){
     function(position){
       const {latitude} = position.coords;
       const {longitude} = position.coords;
-
       const coords = [latitude, longitude];
-
       const map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(map);
 
-      L.marker(coords).addTo(map)
-          .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      map.on('click', function(mapEvent){
+        const {lat, lng} = mapEvent.latlng;   
+
+        L.marker([lat, lng]).addTo(map)
+          .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup'
+          }))
+          .setPopupContent('Workout')
           .openPopup();
+      });
     }
   )
 }
